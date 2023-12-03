6995a33ead608f63db747214de45fbfb7497d452
Oleksii Mikhno <xioflesh@gmail.com>
2023-12-04 00:44:39 +0200

    rename class and commit develope link to graphql

--- app/graphql/github_user_schema.rb
+++ app/graphql/github_user_schema.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RoRGetRepoDataFromUserSchema < GraphQL::Schema
+class GithubUserSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
