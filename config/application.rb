a2c15d93ee3acc1e4300bf4a5d95ae59d13c898e
Oleksii Mikhno <xioflesh@gmail.com>
2023-12-03 19:24:00 +0200

    add deploy config

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module RoRGetRepoDataFromUser
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    Dotenv.load(File.join(Rails.root, 'config', '.env'))
   end
 end
---
+++ config/deploy/production.rb
@@ -0,0 +1,10 @@
+server '164.92.192.74', user: "#{fetch(:user)}", roles: %w{app db web}, primary: true
+
+set :application, 'RoR-get-repo-data-from-user'
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+
+set :environment, 'production'
+set :rails_env,   'production'
+
+set :nginx_server_name, '164.92.192.74'
+set :puma_conf, "#{shared_path}/config/puma.rb"
